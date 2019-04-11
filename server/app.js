2534ca44ee25577b7391a4037a8022e8c003828d
xiaofengbai <byf20222@163.com>
2019-04-11 20:47:30 +0800

    test

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 "use strict";
 let config = require('./config/base');
 let moment = require('moment');
-// let logger = require('./core/log').logger('app');
 let db = require("./core/mongo").init();
 let app = require("./core/express")(db);
-// app.use(logger.connect('app', 'info'));
+let path = require('path');
+let express = require('express');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '../public')));
 app.listen(config.port, config.ip, () => {
     console.log('connect', `server ${config.ip+ ':' + config.port} created`)
 })
